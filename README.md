64f738131adc40846d6397f206ea82fe061f00a1
emadbakry <101658737+emadbakry@users.noreply.github.com>
2022-12-06 00:24:46 +0300

    add_readme_file

---
+++ README.md
@@ -0,0 +1,15 @@
+#WELCOME
+
+![Preview](./overview.png)
+
+
+This is a small page for a Shawarma meal restaurant!
+
+I made it for a client seen in Google Map.
+
+I used:
+-bootstrap 5.2.3
+-Fontawesome 6.2
+-HTML,CSS and JS
+
+If you want to see it live [Click Here](https://emadbakry.github.io/restaurant_shawarma/)
--- js/index.js
+++ js/index.js
@@ -8,14 +8,11 @@ let btns = document.querySelectorAll('.main_nav li');
 let change_catag = (ele) => {
     let recent = document.querySelector('#products.active');
     recent.classList.add("hide");
-    
     setTimeout(() => {
         btns.forEach((btn) => btn.classList.remove('active'));
         ele.classList.add('active');
         let categ_name = ele.getAttribute('data-catag');
         let categ_to_active = document.querySelector(`#products.${categ_name}`);
-        
-        
         categories.forEach((categs) => {
             categs.classList.remove('active');
             categs.classList.remove('hide');
